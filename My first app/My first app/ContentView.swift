cc8cb36740146b391abc61308e50c029f32396e8
balmutaw3 <100786689+balmutaw3@users.noreply.github.com>
2022-03-11 22:45:05 +0300

    Done
    Do

---
+++ My first app/My first app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My first app
+//
+//  Created by Bader Al Mutawa on 11/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello,Bader!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My first app/My first app/My_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_first_appApp.swift
+//  My first app
+//
+//  Created by Bader Al Mutawa on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
